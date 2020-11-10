6ca84237d8d95afeec82007f920a3ee40134b449
SaraWin98 <44393517+SaraWin98@users.noreply.github.com>
2020-11-10 11:46:56 +0100

    Changes

---
+++ src/sample/Grade.java
@@ -0,0 +1,45 @@
+package sample;
+
+public class Grade {
+    private String grade;
+    private String studentID;
+    private String courseName;
+
+    public Grade(String grade, String studentID) {
+        this.grade = grade;
+        this.studentID = studentID;
+    }
+
+    public Grade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return grade;
+    }
+}
---
+++ src/sample/Student.java
@@ -0,0 +1,60 @@
+package sample;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private String hometown;
+
+    public Student(String firstName, String lastName, String studentID, String hometown) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.hometown = hometown;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String info() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", studentID='" + studentID + '\'' +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
